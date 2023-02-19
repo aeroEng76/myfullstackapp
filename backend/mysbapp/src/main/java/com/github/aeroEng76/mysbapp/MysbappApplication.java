2113c507a3806ceff539d2e6e84670b66aafa5d4
aeroEng76 <errosmuh@gmail.com>
2023-02-18 22:44:55 -0800

    add springboot

---
+++ backend/mysbapp/src/main/java/com/github/aeroEng76/mysbapp/MysbappApplication.java
@@ -0,0 +1,13 @@
+package com.github.aeroEng76.mysbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysbappApplication.class, args);
+	}
+
+}
---
+++ backend/mysbapp/src/test/java/com/github/aeroEng76/mysbapp/MysbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aeroEng76.mysbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
